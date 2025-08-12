45bdad4d6692c4eabc137d5e012d508548cf3766
Kevin Anderson <kraatw2008@yahoo.com>
2025-08-12 12:36:10 -0500

    python version is feature matched with c#

--- crossPost.py
+++ crossPost.py
@@ -5,12 +5,12 @@ from difflib import SequenceMatcher
 from PIL import Image
 import requests
 from io import BytesIO
-import skia-python
 
 with open('creds.json','r') as file:
     creds = json.load(file)
 
-with open('CrossPosted.txt','r') as file:
+crossPostedListName = 'CrossPosted.txt'
+with open(crossPostedListName,'r') as file:
     crossPosted = file.read()
 
 reddit = praw.Reddit(
@@ -21,31 +21,58 @@ reddit = praw.Reddit(
     user_agent="windows:com.kra2008.stereomancerbot:v2 (by /u/kra2008)"
 )
 
-postsLimit = 15
-originPosts = reddit.subreddit("crossview").new(limit=postsLimit)
-destinationPostTitles = [post.title for post in reddit.subreddit("parallelview").new(limit=postsLimit)]
+def swapAndCrossPost(originSubName,destinationSubName):
+    postsLimit = 1
+    originSubreddit = reddit.subreddit(originSubName)
+    destinationSubreddit = reddit.subreddit(destinationSubName)
+    originPosts = originSubreddit.new(limit=postsLimit)
+    destinationPostTitles = [post.title for post in destinationSubreddit.new(limit=postsLimit)]
 
-def duplicatesFilter(post):
-    for title in destinationPostTitles:
-        if SequenceMatcher(None, post.title, title).ratio() > 0.8:
-            return True
-    return False
+    def duplicatesFilter(post):
+        for title in destinationPostTitles:
+            if SequenceMatcher(None, post.title, title).ratio() > 0.8:
+                return True
+        return False
 
-eligiblePosts = [post for post in originPosts 
-                 if post.is_video == False and                          #keep videos out for now
-                 hasattr(post,'is_gallery') == False and                #keep galleries out for now
-                 post.url.endswith('.jpeg') and
-                 post.id not in crossPosted and
-                 post.author.name not in creds['OptedOutUsers'] and
-                 post.author.name != 'StereomancerBot' and
-                 post.upvote_ratio > 0.5 and
-                 duplicatesFilter(post) == False]
+    eligiblePosts = [post for post in originPosts 
+                     if post.is_video == False and                          #keep videos out for now
+                     hasattr(post,'is_gallery') == False and                #keep galleries out for now
+                     post.url.endswith('.jpeg') and
+                     post.id not in crossPosted and
+                     post.author.name not in creds['OptedOutUsers'] and
+                     post.author.name != 'StereomancerBot' and
+                     post.upvote_ratio > 0.5 and
+                     duplicatesFilter(post) == False]
 
-for post in eligiblePosts:
-    print(post.title)
-    imageResponse = requests.get(post.url)
-    originalImage = Image.open(BytesIO(response.content))
+    for originalPost in eligiblePosts:
+        print(originalPost.title)
+        imageResponse = requests.get(originalPost.url)
+        originalImage = Image.open(BytesIO(imageResponse.content))
+        originalWidth = originalImage.width
+        originalHeight = originalImage.height
+    
+        swappedImage = originalImage.copy()
 
-#     print(post.id)
-#     print(post.author)
-    # pprint.pprint(vars(post))
+        leftStart = (int(originalWidth/-2),0)
+        rightStart = (int(originalWidth/2),0)
+        swappedImage.paste(originalImage,leftStart)
+        swappedImage.paste(originalImage,rightStart)
+
+        tempFileName='temp/temp.jpg'
+        swappedImage.save(tempFileName)
+
+        swappedPost = destinationSubreddit.submit_image(image_path=tempFileName,title=originalPost.title + ' (converted from r/' + originSubName + ')')
+
+        swappedPost.reply("Original post: " + originalPost.permalink + " by [" + originalPost.author.name + "](https://reddit.com/user/" + originalPost.author.name + ")"+
+                                "\r\n\r\n" +
+                                "I'm a bot made by [KRA2008](https://reddit.com/user/KRA2008) to help the stereoscopic 3D community on Reddit :) " +
+                                "I convert posts between cross and parallel viewing and repost them between the two subs. " +
+                                "Please message [KRA2008](https://reddit.com/user/KRA2008) if you have comments or questions.")
+
+        originalPost.reply("I'm a bot made by [KRA2008](https://reddit.com/user/KRA2008) and I've converted this post to r/" + destinationSubName + " and you can see that here: " + swappedPost.permalink)
+
+        with open(crossPostedListName,'a') as file:
+            file.write(originalPost.id+'\n')
+
+swapAndCrossPost('crossview','parallelview')
+swapAndCrossPost('parallelview','crossview')
